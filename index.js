59d20fe834d43900c22f02af33ec76100f361ad9
hanyu <bhanyu3006@gmail.com>
2023-07-01 11:18:40 +0800

    update time in real-time

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Enable Day.js plugins
     dayjs.extend(dayjs_plugin_utc);
     dayjs.extend(dayjs_plugin_timezone);
-    
-    // Get the current timezone using Day.js
-    const currentTimezone = dayjs.tz.guess();
-    const currentTime = dayjs().format('h:mm:ss A')
-    const currentDate = dayjs().format('dddd, MMMM D YYYY')
   
-    // Update the HTML element with the current timezone
-    document.getElementById('timezone').textContent = currentTimezone;
-    document.getElementById('time').textContent = currentTime;
-    document.getElementById('date').textContent = currentDate;
+    // Function to update the time and date
+    function updateTimeAndDate() {
+      // Get the current timezone using Day.js
+      const currentTimezone = dayjs.tz.guess();
+      const currentTime = dayjs().format('h:mm:ss A');
+      const currentDate = dayjs().format('dddd, MMMM D YYYY');
+  
+      // Update the HTML elements with the current timezone, time, and date
+      document.getElementById('timezone').textContent = currentTimezone;
+      document.getElementById('time').textContent = currentTime;
+      document.getElementById('date').textContent = currentDate;
+    }
+  
+    // Initial update
+    updateTimeAndDate();
+  
+    // Update the time and date every second (1000 milliseconds)
+    setInterval(updateTimeAndDate, 1000);
   });
   
