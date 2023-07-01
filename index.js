21d1184b130986a88950db839e5f3a189c0fb668
hanyu <bhanyu3006@gmail.com>
2023-07-01 13:06:18 +0800

    close modal and display chosen timezone

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ document.addEventListener("DOMContentLoaded", function() {
     dayjs.extend(dayjs_plugin_utc);
     dayjs.extend(dayjs_plugin_timezone);
 
+    // Store the selected timezone
+    var selectedTimezone = null;
+
     // Function to update the time and date
     function updateTimeAndDate() {
-      // Get the current timezone using Day.js
-      const currentTimezone = dayjs.tz.guess();
-      const currentTime = dayjs().format('h:mm:ss A');
-      const currentDate = dayjs().format('dddd, MMMM D YYYY');
-  
-      // Update the HTML elements with the current timezone, time, and date
-      document.getElementById('timezone').textContent = currentTimezone;
-      document.getElementById('time').textContent = currentTime;
-      document.getElementById('date').textContent = currentDate;
+        // Get the current timezone using Day.js
+        var currentTimezone = selectedTimezone || dayjs.tz.guess();
+        var currentTime = dayjs().tz(currentTimezone).format('h:mm:ss A');
+        var currentDate = dayjs().tz(currentTimezone).format('dddd, MMMM D YYYY');
+
+        // Update the HTML elements with the current timezone, time, and date
+        document.getElementById('timezone').textContent = currentTimezone;
+        document.getElementById('time').textContent = currentTime;
+        document.getElementById('date').textContent = currentDate;
     }
-  
+
     // Initial update
     updateTimeAndDate();
-  
+
     // Update the time and date every second (1000 milliseconds)
     setInterval(updateTimeAndDate, 1000);
 
@@ -26,6 +29,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById('timezone').addEventListener('click', function() {
         MicroModal.show('modal-1');
-      });
-    
+    });
+
+    // Get the necessary elements
+    var timezoneElement = document.getElementById("timezone");
+    var timeElement = document.getElementById("time");
+    var dateElement = document.getElementById("date");
+    var selectElement = document.getElementById("timezoneSelect");
+
+    // Update timezone, time, and date based on the selected option
+    selectElement.addEventListener("change", function() {
+        var selectedOption = selectElement.value;
+        selectedTimezone = selectedOption;
+
+        timezoneElement.textContent = selectedOption;
+        timeElement.textContent = dayjs().tz(selectedOption).format("HH:mm:ss");
+        dateElement.textContent = dayjs().tz(selectedOption).format("dddd, MMMM D, YYYY");
+
+        // Close the modal
+        MicroModal.close("modal-1");
+    });  
 });
